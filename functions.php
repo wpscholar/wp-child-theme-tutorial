76c9f804e3bd2d5685d777414d869a431f2801be
Micah Wood <micah@wpscholar.com>
2015-11-12 11:29:29 -0500

    Register a custom sidebar.  We are using the 'widgets_init' action hook to register our callback function.  Our callback function calls `register_sidebar()`, which can take a number of arguments in the form of an array.  We set the 'id' so we will have an internal handle by which we can refer to this particular sidebar.  The name and description are pretty self-explanatory and are shown to users in the admin area.  The 'before_widget' and 'after_widget' allow us to customize the markup in which our widgets are wrapped.  By default, WordPress will wrap widgets in an LI element.  I've updated the code so our widgets are wrapped in aside elements in the same way other widgets in TwentyFifteen are wrapped.
    Now, be sure to go and see the new sidebar you added in the WordPress admin on the 'Widgets' page.  While you are at it, drag a couple of widgets into it.

--- functions.php
+++ functions.php
@@ -6,6 +6,18 @@ function tfc_after_setup_theme() {
 	register_nav_menu( 'footer_menu', __( 'Footer Menu', 'twentyfifteen-child' ) );
 }
 
+add_action( 'widgets_init', 'tfc_widgets_init' );
+
+function tfc_widgets_init() {
+	register_sidebar( array(
+		'id'            => 'footer-widget-area',
+		'name'          => __( 'Footer Widget Area', 'twentyfifteen-child' ),
+		'description'   => __( 'Add widgets here to appear in your footer.', 'twentyfifteen-child' ),
+		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+		'after_widget'  => '</aside>',
+	) );
+}
+
 add_action( 'wp_enqueue_scripts', 'tfc_wp_enqueue_scripts' );
 
 function tfc_wp_enqueue_scripts() {
