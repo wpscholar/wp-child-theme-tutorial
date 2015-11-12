926c81f3c8eb5fbdf56b5418f27190bc89cb5ea7
Micah Wood <micah@wpscholar.com>
2015-11-12 15:06:46 -0500

    Replace the call to `get_template_part()` with the contents of the called template (i.e. content-page.php).

--- sitemap-template.php
+++ sitemap-template.php
@@ -12,7 +12,32 @@ the_post();
 <div id="primary" class="content-area">
 	<main id="main" class="site-main" role="main">
 
-		<?php get_template_part( 'content', 'page' ); ?>
+		<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+
+			<?php twentyfifteen_post_thumbnail(); ?>
+
+			<header class="entry-header">
+				<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+			</header><!-- .entry-header -->
+
+			<div class="entry-content">
+				<?php the_content(); ?>
+				<?php
+				wp_link_pages( array(
+					'before'      => '<div class="page-links"><span class="page-links-title">' . __( 'Pages:', 'twentyfifteen' ) . '</span>',
+					'after'       => '</div>',
+					'link_before' => '<span>',
+					'link_after'  => '</span>',
+					'pagelink'    => '<span class="screen-reader-text">' . __( 'Page', 'twentyfifteen' ) . ' </span>%',
+					'separator'   => '<span class="screen-reader-text">, </span>',
+				) );
+				?>
+			</div><!-- .entry-content -->
+
+			<?php edit_post_link( __( 'Edit', 'twentyfifteen' ), '<footer class="entry-footer"><span class="edit-link">', '</span></footer><!-- .entry-footer -->' ); ?>
+
+		</article><!-- #post-## -->
+
 
 	</main><!-- .site-main -->
 </div><!-- .content-area -->
