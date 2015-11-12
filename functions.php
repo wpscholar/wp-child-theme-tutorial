911444cdfed9d54243e1d914915993924f7d4a44
Micah Wood <micah@wpscholar.com>
2015-11-12 13:38:23 -0500

    Adding our own section and setting to the theme customizer.
    We are using the `customize_register` action hook, which gives us a variable named `$wp_customize` that is an instance of the WP_Customize_Manager class.  We call the `add_section()` method to add our custom section.  We call the `add_setting()` method to add our custom setting.  The ID you assign the setting will be the same string you use with `get_theme_mod()` to fetch the setting's value.  Finally, we call the `add_control()` method which allows us to define what type of field (or more advanced control) will be used to control our setting.  Note that when adding a control, you must define the 'section' and 'settings' parameters using the IDs of your section and setting respectively.
    
    Now, visit the theme customizer in the WordPress admin and check out your new setting!

--- functions.php
+++ functions.php
@@ -24,3 +24,25 @@ function tfc_wp_enqueue_scripts() {
 	wp_enqueue_style( 'twentyfifteen-style', get_template_directory_uri() . '/style.css' );
 	wp_enqueue_style( 'twentyfifteen-child-style', get_stylesheet_directory_uri() . '/style.css' );
 }
+
+add_action( 'customize_register', 'tfc_customize_register' );
+
+function tfc_customize_register( WP_Customize_Manager $wp_customize ) {
+	$wp_customize->add_section( 'section_archives', array(
+		'title' => __( 'Archives', 'twentyfifteen-child' ),
+	) );
+	$wp_customize->add_setting( 'show_excerpts_in_archives', array(
+		'type'      => 'theme_mod',
+		'transport' => 'refresh',
+	) );
+	$wp_customize->add_control( new WP_Customize_Control(
+		$wp_customize,
+		'show_excerpts_in_archives_control',
+		array(
+			'label'    => __( 'Only show excerpts on archive pages', 'twentyfifteen-child' ),
+			'section'  => 'section_archives',
+			'settings' => 'show_excerpts_in_archives',
+			'type'     => 'checkbox',
+		)
+	) );
+}
