ec238a0120661f5f0cbc6b0a5c896114e93e0105
Micah Wood <micah@wpscholar.com>
2015-11-12 15:04:56 -0500

    Delete the loop. WAT?!!?!
    Yep. On singular pages, posts and custom post types you can just call `the_post()`.  No need for a loop if there is only one!

--- sitemap-template.php
+++ sitemap-template.php
@@ -4,21 +4,15 @@
  * Template Name: Sitemap Template
  */
 
-get_header(); ?>
+get_header();
+the_post();
+
+?>
 
 <div id="primary" class="content-area">
 	<main id="main" class="site-main" role="main">
 
-		<?php
-		// Start the loop.
-		while ( have_posts() ) : the_post();
-
-			// Include the page content template.
-			get_template_part( 'content', 'page' );
-
-			// End the loop.
-		endwhile;
-		?>
+		<?php get_template_part( 'content', 'page' ); ?>
 
 	</main><!-- .site-main -->
 </div><!-- .content-area -->
